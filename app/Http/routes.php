a41fe3cfc562e7e833bcd18b3bb378bdc07d0a4e
harryxu <harryzhxu@gmail.com>
2016-06-27 02:03:54 +0800

    Base api construct setup.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use Dingo\Api\Routing\Router;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/**
+ * @var Dingo\Api\Routing\Router
+ */
+$api = app(Router::class);
+
+$api->version('v1', function(Router $api) {
+    $api->resource('topic', \App\Http\Controllers\TopicController::class);
 });
--- config/app.php
+++ config/app.php
@@ -156,6 +156,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Dingo\Api\Provider\LaravelServiceProvider::class,
     ],
 
     /*
